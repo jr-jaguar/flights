085376a589c823f128cbd5215e2a9ec2acb34e0d
Jack Jaguar <j.jaguar55@gmail.con>
2023-07-21 00:36:40 +0300

    Changed EuropeanCountry to Country

--- Nginx/www/app/Domain/Services/CountryProvider.php
+++ Nginx/www/app/Domain/Services/CountryProvider.php
@@ -4,7 +4,7 @@ namespace App\Domain\Services;
 
 use App\Contracts\CountryProviderInterface;
 
-class EuropeanCountryProvider implements CountryProviderInterface
+class CountryProvider implements CountryProviderInterface
 {
     private $europeanCountries = ['EU', 'LT', 'LV', 'EE', 'DE', 'FR', 'IT', 'ES', 'PT', 'PL', 'GR', 'AT', 'BE', 'NL', 'LU', 'SI', 'MT', 'CY', 'SK', 'HU', 'CZ', 'HR', 'BG', 'RO', 'SE', 'FI', 'DK', 'IE'];
 
