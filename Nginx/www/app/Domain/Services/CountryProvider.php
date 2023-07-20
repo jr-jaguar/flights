dfa51f9a8a82c58287916ba5bf7047afb40f5099
Jack Jaguar <j.jaguar55@gmail.con>
2023-07-21 00:33:35 +0300

    Added Country provider

---
+++ Nginx/www/app/Domain/Services/CountryProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Domain\Services;
+
+use App\Contracts\CountryProviderInterface;
+
+class EuropeanCountryProvider implements CountryProviderInterface
+{
+    private $europeanCountries = ['EU', 'LT', 'LV', 'EE', 'DE', 'FR', 'IT', 'ES', 'PT', 'PL', 'GR', 'AT', 'BE', 'NL', 'LU', 'SI', 'MT', 'CY', 'SK', 'HU', 'CZ', 'HR', 'BG', 'RO', 'SE', 'FI', 'DK', 'IE'];
+
+    public function isEuropeanCountry(string $countryCode): bool
+    {
+        return in_array($countryCode, $this->europeanCountries);
+    }
+}
